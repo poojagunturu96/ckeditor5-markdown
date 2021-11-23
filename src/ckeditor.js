c33363858f0ad7f705333fdc132c81e2216e044a
poojagunturu96 <pgunturu@middlebury.edu>
2021-11-23 13:10:59 -0600

    Added HorizontalLine plugin

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -7,27 +7,20 @@ import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat.js';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
+import HorizontalLine from '@ckeditor/ckeditor5-horizontal-line/src/horizontalline';
 import Image from '@ckeditor/ckeditor5-image/src/image.js';
 import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption.js';
 import Indent from '@ckeditor/ckeditor5-indent/src/indent.js';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
 import Link from '@ckeditor/ckeditor5-link/src/link.js';
 import List from '@ckeditor/ckeditor5-list/src/list.js';
-// import Markdown from '@ckeditor/ckeditor5-markdown-gfm/src/markdown.js';
-import GFMDataProcessor from '@ckeditor/ckeditor5-markdown-gfm/src/gfmdataprocessor';
+import Markdown from '@ckeditor/ckeditor5-markdown-gfm/src/markdown.js';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
 import Table from '@ckeditor/ckeditor5-table/src/table.js';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar.js';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation.js';
 
-class Editor extends ClassicEditor {
-	constructor () {
-		super();
-		const dp = new GFMDataProcessor(this.data.viewDocument);
-		dp.keepHtml( 'a' );
-		this.data.processor = dp;
-	}
-}
+class Editor extends ClassicEditor {}
 
 // Plugins to include in the build.
 Editor.builtinPlugins = [
@@ -35,13 +28,14 @@ Editor.builtinPlugins = [
 	Bold,
 	Essentials,
 	Heading,
+	HorizontalLine,
 	Image,
 	ImageCaption,
 	Indent,
 	Italic,
 	Link,
 	List,
-	// Markdown,
+	Markdown,
 	Paragraph,
 	Table,
 	TableToolbar,
@@ -63,6 +57,7 @@ Editor.defaultConfig = {
 			'indent',
 			'|',
 			'insertTable',
+			'horizontalLine',
 			'undo',
 			'redo'
 		]
